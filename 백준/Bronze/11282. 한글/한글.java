53dffaf9e81d28e85ab97edf95c854745f3cfb8f
ki-sd <redringo4@gmail.com>
2026-03-26 14:15:30 +0900

    [Bronze IV] Title: 한글, Time: 176 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        char c=(char)(n+44031);
+        System.out.println(c);
+    }
+}
