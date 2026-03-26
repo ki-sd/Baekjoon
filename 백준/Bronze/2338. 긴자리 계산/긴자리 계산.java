c19ade7a0a6315d4909e86cefa90f6aaf6d731f7
ki-sd <redringo4@gmail.com>
2026-03-26 14:16:29 +0900

    [Bronze V] Title: 긴자리 계산, Time: 192 ms, Memory: 17880 KB -BaekjoonHub

---
+++ 백준/Bronze/2338. 긴자리 계산/긴자리 계산.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+        BigInteger sum = A.add(B);
+        System.out.println(sum);
+        BigInteger sub = A.subtract(B);
+        System.out.println(sub);
+        BigInteger mul = A.multiply(B);
+        System.out.println(mul);
+    }
+}
