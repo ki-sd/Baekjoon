81da5e7695876099d09f63d278758d0637b77281
ki-sd <redringo4@gmail.com>
2026-04-01 09:51:21 +0900

    [Bronze V] Title: 검증수, Time: 168 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.math.*;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int sum=0;
+        for(int i=0;i<5;i++){
+            int num=sc.nextInt();
+            sum+=(int)Math.pow(num,2);
+        }
+        System.out.println(sum%10);
+    }
+}
